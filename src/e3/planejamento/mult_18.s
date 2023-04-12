3a4ff045de2ee6e8f50e4da8898e7e3650cb81ce
Bruno Mariz <bruno.mariz@semantix.inc>
2023-04-12 15:53:17 -0300

    add comentarios

--- src/e3/planejamento/mult_18.s
+++ src/e3/planejamento/mult_18.s
@@ -2,9 +2,9 @@
 	.globl	main 
 main:
 	MOV 	r4, #0x1
-	RSB 	r3, r4, r4, LSL #2	
-	ADD 	r4, r3, r3, LSL #2	
-	ADD 	r4, r4, r3
+	RSB 	r3, r4, r4, LSL #2	@ r3 = r4 * 3
+	ADD 	r4, r3, r3, LSL #2	@ r4 = r3 * 5  = r4 * 15
+	ADD 	r4, r4, r3          @ r4 = r4 + r3 = r4 * 15 + r4 * 3 = r4 * 18
 	BL 	fim
 	MOV	r0, #0x18
 	LDR	r1, =0x20026
